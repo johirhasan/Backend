57343e8205b64866858d7b7ca7270a25aedb198a
johirhasan <johirhasan10203@gmail.com>
2024-04-08 02:09:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/facebook',(req ,res) =>
+    {res.send('https://www.facebook.com/Jahir.shourov')}
+)
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
